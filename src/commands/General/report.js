094ff32a6510f5fe3b53d72739b352ca484c745c
Stress_Giver <115838946+Push-s@users.noreply.github.com>
2023-07-24 01:12:07 +0200

    Update report.js

--- src/commands/General/report.js
+++ src/commands/General/report.js
@@ -14,7 +14,20 @@ module.exports = {
             let userTag = m.sender.split("@")[0];
             let userMess = args.join(" ");
             let userName = pushName;
+
+            try {
+                userPfp = await client.profilePictureUrl(m.sender, "image");
+              } catch (e) {
+                userPfp = botImage3;
+              }
+
+            let reportMessage = `              *「 Report Recieved 」*\n\n*👤 Reported By:* @${userTag}\n\n*📝 Message:* ${userMess}\n\n*📅 Date:* ${moment().tz('Asia/Kolkata').format('DD/MM/YYYY')}\n*⏰ Time:* ${moment().tz('Asia/Kolkata').format('hh:mm:ss A')}\n*🍁 Character using:* Ari-Ani\n\n*📌 Note: This is an automated message, please do not reply to this message to avoid blocking.*`;
+            m.reply(`Sending report to main developer...\n\nIf it is a spam you might get *blocked* and *banned*.`);
             
+            let devs = [`263788671478@s.whatsapp.net`]
+
+            for (let i = 0; i < devs.length; i++) {
+              await client.sendMessage(devs[i],{image: {url: userPfp}, caption: reportMessage,mentions: [m.sender],});
             }
         }
         else{
@@ -22,14 +35,20 @@ module.exports = {
             let userTag = m.sender.split("@")[0];
             let userMess = args.join(" ");
             let userName = pushName;
-            let gcName = metadata.subject
-            let reportMessage = `  *「 Report Recieved 」*\n\n*👤 Reported By:* @${userTag}\n*🧩 Group Name:* ${gcName}\n\n*📝 Message:* ${userMess}\n\n*📅 Date:* ${moment().tz('Asia/Kolkata').format('DD/MM/YYYY')}\n*⏰ Time:* ${moment().tz('Asia/Kolkata').format('hh:mm:ss A')}\n*🍁 Character using:* Ari-Ani\n\n*📌 Note: This is an automated message, please do not reply to this message to avoid blocking.*`;
+            let gcName = metadata.subject;
+
+            try {
+                ppgc = await client.profilePictureUrl(m.from, "image");
+              } catch {
+                ppgc = botImage3;
+              }
+              let reportMessage = `              *「 Report Recieved 」*\n\n*👤 Reported By:* @${userTag}\n*🧩 Group Name:* ${gcName}\n\n*📝 Message:* ${userMess}\n\n*📅 Date:* ${moment().tz('Asia/Kolkata').format('DD/MM/YYYY')}\n*⏰ Time:* ${moment().tz('Asia/Kolkata').format('hh:mm:ss A')}\n*🍁 Character using:* Ari-Ani\n\n*📌 Note: This is an automated message, please do not reply to this message to avoid blocking.*`;
               m.reply(`Sending report to main developer...\n\nIf it is a spam you might get *blocked* and *banned*.`);
 
-              let devs = [`263788671478@swhatsapp.net`]
+              let devs = [`263788671478@s.whatsapp.net`]
 
               for (let i = 0; i < devs.length; i++) {
-                await client.sendMessage(devs[i],{image: reportMessage,mentions: [m.sender],});
+                await client.sendMessage(devs[i],{image: {url: ppgc}, caption: reportMessage,mentions: [m.sender],});
             }
         }
     }
