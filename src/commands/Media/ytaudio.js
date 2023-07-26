7172d96c21b110b61493280c586681d2f0e469d6
Stress_Giver <115838946+Push-s@users.noreply.github.com>
2023-07-26 23:04:09 +0200

    Update ytaudio.js

--- src/commands/Media/ytaudio.js
+++ src/commands/Media/ytaudio.js
@@ -1,58 +1,63 @@
 const YT = require('../../lib/YT')
-// const yts = require('yt-search')
-const yts = require("youtube-yts");
+const yts = require('yt-search')
 
 module.exports = {
     name: 'ytaudio',
-    aliases: ['yta', 'play'],
+    aliases: ['yta'],
     category: 'media',
-     react:"🎵",
     description: 'Downloads given YT Video and sends it as Audio',
+    react: "✅",
     async execute(client, arg, M) {
+       try{
         const link = async (term) => {
-            const { videos } = await yts(term.trim())
-            if (!videos || !videos.length) return null
-            return videos[0].url
-        }
-        if (!arg) return M.reply('Please use this command with a valid youtube.com link')
-        const validPathDomains = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/
-        const term = validPathDomains.test(arg) ? arg.trim() : await link(arg)
-        if (!term) return M.reply('Please use this command with a valid youtube contant link')
-        if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid youtube.com link')
-        const { videoDetails } = await YT.getInfo(term)
-        M.reply('*Please wait by Deryl...*')
-        let text = `*Title:* ${videoDetails.title} | *Type:* Audio | *From:* ${videoDetails.ownerChannelName}`
-        client.sendMessage(
-            M.from,
-            {
-                image: {
-                    url: `https://i.ytimg.com/vi/${videoDetails.videoId}/maxresdefault.jpg`
-                },
-                caption: text
-            },
-            {
-                quoted: M
-            }
-        )
-        if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('Cannot download audio longer than 30 minutes')
-        const audio = YT.getBuffer(term, 'audio')
-            .then(async (res) => {
-                await client.sendMessage(
-                    M.from,
-                    {
-                        audio: res,
-                        mimetype: 'audio/mpeg',
-                        fileName: videoDetails.title + '.mp3'
-                    },
-                    {
-                        quoted: M
-                    }
-                )
-            })
-            .catch((err) => {
-                return M.reply(err.toString())
-                client.log(err, 'red')
-            })
+          const { videos } = await yts(term.trim())
+          if (!videos || !videos.length) return null
+          return videos[0].url
+      }
+      if (!arg) return M.reply('❌ Please provide a valid YouTube link to get the lyrics.')
+      const validPathDomains = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/
+      const term = validPathDomains.test(arg) ? arg.trim() : await link(arg)
+      if (!term) return M.reply('❌ Please use this command with a valid youtube contant link')
+      if (!YT.validateURL(term.trim())) return M.reply('❌ Please provide a valid YouTube link to get the lyrics.')
+      const { videoDetails } = await YT.getInfo(term)
+      M.reply('🎵 processing your request...')
+      let text = `🎵 *_Title:_* ${videoDetails.title} | 🎧 *_Type:_* Audio | 🎤 *_From:_* ${videoDetails.ownerChannelName}`
+      client.sendMessage(
+          M.from,
+          {
+              image: {
+                  url: `https://i.ytimg.com/vi/${videoDetails.videoId}/maxresdefault.jpg`
+              },
+              caption: text
+          },
+          {
+              quoted: M
+          }
+      )
+      if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('❌ Cannot download audio longer than 30 minutes')
+      const audio = YT.getBuffer(term, 'audio')
+          .then(async (res) => {
+              await client.sendMessage(
+                  M.from,
+                  {
+                      document: res,
+                      mimetype: 'audio/mpeg',
+                      fileName: videoDetails.title + '.mp3'
+                  },
+                  {
+                      quoted: M
+                  }
+              )
+          })
+          .catch((err) => {
+              return M.reply(err.toString())
+              client.log(err, 'red')
+          })
+       }
+            //Our beloved error chan. No one can stop her!
+    catch(err){
+      await client.sendMessage(M.from , {image: {url: `${client.utils.errorChan()}`} , caption: `${client.utils.greetings()} Error-Chan Dis\n\nError:\n${err}`})
+    }
     }
 }
 //M.quoted.mtype === 'imageMessage',
