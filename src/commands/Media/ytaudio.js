02371bbf0c3cf20a46e06ca4cd1643b6e3e93be6
Stress_Giver <115838946+Push-s@users.noreply.github.com>
2023-07-26 23:18:26 +0200

    Update ytaudio.js

--- src/commands/Media/ytaudio.js
+++ src/commands/Media/ytaudio.js
@@ -1,63 +1,68 @@
-const YT = require('../../lib/YT')
-const yts = require('yt-search')
-
+const YT = require('../../lib/ytdl')
+const {
+    isUrl,
+    fetchBuffer
+} = require('../../lib/Function')
+const fs = require("fs")
+const yts = require("yt-search")
+require('../../../settings')
 module.exports = {
-    name: 'ytaudio',
-    aliases: ['yta'],
-    category: 'media',
-    description: 'Downloads given YT Video and sends it as Audio',
+    name: "ytaudio",
+    alias: ["playa", "yta", "mp3"],
+    usage: `${prefa}play <query>`,
+    desc: "Plays the song...",
+    category: "Media",
     react: "✅",
-    async execute(client, arg, M) {
-       try{
-        const link = async (term) => {
-          const { videos } = await yts(term.trim())
-          if (!videos || !videos.length) return null
-          return videos[0].url
-      }
-      if (!arg) return M.reply('❌ Please provide a valid YouTube link to get the lyrics.')
-      const validPathDomains = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/
-      const term = validPathDomains.test(arg) ? arg.trim() : await link(arg)
-      if (!term) return M.reply('❌ Please use this command with a valid youtube contant link')
-      if (!YT.validateURL(term.trim())) return M.reply('❌ Please provide a valid YouTube link to get the lyrics.')
-      const { videoDetails } = await YT.getInfo(term)
-      M.reply('🎵 processing your request...')
-      let text = `🎵 *_Title:_* ${videoDetails.title} | 🎧 *_Type:_* Audio | 🎤 *_From:_* ${videoDetails.ownerChannelName}`
-      client.sendMessage(
-          M.from,
-          {
-              image: {
-                  url: `https://i.ytimg.com/vi/${videoDetails.videoId}/maxresdefault.jpg`
-              },
-              caption: text
-          },
-          {
-              quoted: M
-          }
-      )
-      if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('❌ Cannot download audio longer than 30 minutes')
-      const audio = YT.getBuffer(term, 'audio')
-          .then(async (res) => {
-              await client.sendMessage(
-                  M.from,
-                  {
-                      document: res,
-                      mimetype: 'audio/mpeg',
-                      fileName: videoDetails.title + '.mp3'
-                  },
-                  {
-                      quoted: M
-                  }
-              )
-          })
-          .catch((err) => {
-              return M.reply(err.toString())
-              client.log(err, 'red')
-          })
-       }
-            //Our beloved error chan. No one can stop her!
-    catch(err){
-      await client.sendMessage(M.from , {image: {url: `${client.utils.errorChan()}`} , caption: `${client.utils.greetings()} Error-Chan Dis\n\nError:\n${err}`})
-    }
+    start: async (client, m, {
+        command,
+        prefix,
+        text,
+        args
+    }) => {
+        const link = async (terms) => {
+            const {
+                videos
+            } = await yts(terms)
+            if (!videos || !videos.length) return null
+            return videos[0].url
+        }
+        if (!text) return m.reply('❌ Please provide a valid YouTube link to get the lyrics.')
+        const validPathDomains = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/
+        const term = validPathDomains.test(text) ? text.trim() : await link(text)
+        if (!term) return m.reply('❌ Please use this command with a valid youtube contant link')
+        if (!YT.validateURL(term.trim())) return m.reply('❌ Please provide a valid YouTube link to get the lyrics.')
+        const {
+            videoDetails
+        } = await YT.getInfo(term)
+        m.reply('🔍 Searching for your request...')
+        let caption = `🎵 *_Title:_* ${videoDetails.title} | 🎧 *_Type:_* Audio | 🎤 *_From:_* ${videoDetails.ownerChannelName}`
+        client.sendMessage(
+            m.from, {
+                image: {
+                    url: `https://i.ytimg.com/vi/${videoDetails.videoId}/maxresdefault.jpg`
+                },
+                caption
+            }, {
+                quoted: m
+            }
+        )
+        if (Number(videoDetails.lengthSeconds) > 1800) return m.reply('❌ Cannot download audio longer than 30 minutes')
+        const audio = YT.getBuffer(term, 'audio')
+            .then(async (res) => {
+                await client.sendMessage(
+                    m.from, {
+                        document: res,
+                        mimetype: 'audio/mpeg',
+                        fileName: videoDetails.title + '.mp3'
+                    }, {
+                        quoted: m
+                    }
+                )
+            })
+            .catch((err) => {
+                return m.reply(err.toString())
+                client.log(err, 'red')
+            })
     }
 }
-//M.quoted.mtype === 'imageMessage',
+
