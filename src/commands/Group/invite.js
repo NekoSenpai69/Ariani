b48a2d7e9714deef5863f255920f49f64e68b652
Stress_Giver <115838946+Push-s@users.noreply.github.com>
2023-07-31 12:44:46 +0200

    Update invite.js

--- src/commands/Group/invite.js
+++ src/commands/Group/invite.js
@@ -9,11 +9,11 @@ module.exports = {
 	start: async(client, m, { text, groupName,flags, args,command,isAdmin,participants,groupAdmin,pushName,iscreator}) => {
 		const code = await client.groupInviteCode(m.from)
 m.reply("https://chat.whatsapp.com/" + code)
-let dm = '📪Sent you the Group Link in personal message.'
+let dm = '🟦Sent you the Group Link in personal message.'
 let lemo = `*Invite link:* https://chat.whatsapp.com/${code}`
 
 
-await client.sendMessage(m.from,{text: `*${pushName}* have a look in your DM`},{quoted:m})
+await client.sendMessage(m.from,{text: `*${pushName}* ⬜have a look in your DM`},{quoted:m})
   await  client.sendMessage(m.sender,{text:lemo},{quoted:m})  
 
     }
