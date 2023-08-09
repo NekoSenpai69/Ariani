581bfdb8d005ef99fb0ac2189783e18557eaec0f
Stress_Giver <115838946+Push-s@users.noreply.github.com>
2023-08-09 15:11:12 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "923265825610,923087880256";
+  gg = "263788671478";
 }
 
 let owners = gg.split(",");
@@ -11,12 +11,12 @@ global.owner = owners.map(owner => owner + '@s.whatsapp.net');
 global.mongodb = process.env.MONGO_URI || null;
 global.prefa = process.env.PREFIX || ".";
 global.sessionId = process.env.SESSION_ID || null;
-global.malid = process.env.MAL_USERNAME || "Aku_sempai";
+global.malid = process.env.MAL_USERNAME || "Stess_Giver";
 global.malpass = process.env.MAL_PASSWORD || "Larsnoctis7";
 global.port = process.env.PORT || 8080;
-global.packname = process.env.PACKNAME || "Ari Ani";
-global.nameBot = process.env.NAME || "Ari Ani";
-global.economy = process.env.ECONOMY || "true";
+global.packname = process.env.PACKNAME || "Archer";
+global.nameBot = process.env.NAME || "Archer";
+global.economy = process.env.ECONOMY || "false";
 
 let file = require.resolve(__filename);
 fs.watchFile(file, () => {
